e150cf49ffcb0a938a0b8c31b9c7c6f8d5fa88d4
Kurt Monge <kurt.monge@knowit.com>
2024-07-04 14:45:24 +0200

    Gjort endringer i api, laget ps og sh startfiler, utvidet testdata mm.

---
+++ Backend/build_and_run.ps1
@@ -0,0 +1,5 @@
+# Bygg Docker-bildet for IntegrationApi uten cache
+docker-compose build --no-cache integration_api
+
+# Start alle containerne
+docker-compose up
---
+++ Backend/build_and_run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Bygg Docker-bildet for IntegrationApi uten cache
+docker-compose build --no-cache integration_api
+
+# Start alle containerne
+docker-compose up
